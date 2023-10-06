6aaf59d96a891819a53a4bff3a961d43b768f259
Maxime <sin2.chabert.maxime@gmail.com>
2023-10-06 08:42:51 +0200

    add fixed apy card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@
 
 import { Tabs, TabsContent, TabsTrigger } from '@/components/ui/tabs';
 import { TabsList } from '@radix-ui/react-tabs';
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { RevenueChart } from '@/app/_components/revenue-chart.component';
-import { RevenueCompareChart } from '@/app/_components/revenue-compare-chart.component';
+import { AnalyticsTab } from '@/app/_components/analytics-tab.component';
 
 export default function Home() {
   return (
@@ -14,57 +12,31 @@ export default function Home() {
           <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
         </div>
 
-        <Tabs defaultValue="overview" className="space-y-4">
+        <Tabs defaultValue="analytics" className="space-y-4">
           <TabsList>
-            <TabsTrigger value="overview">Overview</TabsTrigger>
-            <TabsTrigger value="simulator" disabled>
-              Simulator
-            </TabsTrigger>
-            <TabsTrigger value="analytics" disabled>
+            <TabsTrigger
+              value="analytics"
+              className="data-[state=active]:font-bold"
+            >
               Analytics
+            </TabsTrigger>
+            <TabsTrigger
+              value="money"
+              className="data-[state=active]:font-bold"
+            >
+              My Money
+            </TabsTrigger>
+            <TabsTrigger value="simulator" disabled>
+              Simulator (coming soon)
             </TabsTrigger>
           </TabsList>
 
-          <TabsContent value="overview" className="space-y-4">
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-              <RevenueChart />
-              <div className="col-span-2">
-                <RevenueCompareChart />
-              </div>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Active Now
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+573</div>
-                  <p className="text-xs text-muted-foreground">
-                    +201 since last hour
-                  </p>
-                </CardContent>
-              </Card>
-            </div>
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-7">
-              <Card className="col-span-4">
-                <CardHeader>
-                  <CardTitle>Overview</CardTitle>
-                </CardHeader>
-                <CardContent>test</CardContent>
-              </Card>
-            </div>
+          <TabsContent value="money" className="space-y-4">
+            My Money
+          </TabsContent>
+
+          <TabsContent value="analytics" className="space-y-4">
+            <AnalyticsTab />
           </TabsContent>
         </Tabs>
       </div>
