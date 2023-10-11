688a00b163af0c52699e88788d5ef0bcf7df4521
Maxime <sin2.chabert.maxime@gmail.com>
2023-10-11 20:00:01 +0200

    use lucide icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { RevenueChart } from '@/app/_components/revenue-chart.component';
 import { RevenueCompareChart } from '@/app/_components/revenue-compare-chart.component';
+import { Percent, DollarSign } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -11,18 +12,7 @@ export default function Home() {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Fixed APY</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
+            <Percent className="h-4 w-4 text-muted-foreground"></Percent>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">6%</div>
@@ -32,18 +22,7 @@ export default function Home() {
         <Card>
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <CardTitle className="text-sm font-medium">Total deposit</CardTitle>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="h-4 w-4 text-muted-foreground"
-            >
-              <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-            </svg>
+            <DollarSign className="h-4 w-4 text-muted-foreground"></DollarSign>
           </CardHeader>
           <CardContent>
             <div className="text-2xl font-bold">3,456,000 $</div>
